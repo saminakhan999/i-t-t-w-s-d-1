a4b94e203bd6d672bb6c04766e9c40ebaf672512
saminakhan999 <samina.khan9999@gmail.com>
2022-03-20 18:14:41 +0000

    chat shows onclick

--- src/pages/PlayGame/PlayGame.js
+++ src/pages/PlayGame/PlayGame.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import TheQuestions from "../../components/TheQuestions";
 import { useSocket } from "../../contexts/SocketContext/SocketContext";
+import QuizChat from "../../components/QuizChat";
 import { recordPlayerResult } from "../../actions";
 
 const PlayGame = () => {
   const dispatch = useDispatch();
   const socket = useSocket();
+  const [showResults, setShowResults] = useState(false);
+  const onClick = () => setShowResults(true);
 
 
 
@@ -20,7 +23,9 @@ const PlayGame = () => {
   return (
     <div>
       <TheQuestions />
-      <div className="bg-slate-50 text-tblack py-3 text-center rounded-full font-bold px-6 mx-auto fixed bottom-16 left-0 right-0 w-fit">Chat</div>
+      <div onClick={onClick} className="bg-slate-50 text-tblack py-3 text-center rounded-full font-bold px-6 mx-auto fixed bottom-16 left-0 right-0 w-fit">
+        {showResults ? <QuizChat/> : null}Chat
+      </div>
     </div>
   );
 };
