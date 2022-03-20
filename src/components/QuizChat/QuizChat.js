c9a84ce1374b7139f07cb7e2cde85776c04a121c
saminakhan999 <samina.khan9999@gmail.com>
2022-03-20 19:19:22 +0000

    created socket chat

--- src/components/QuizChat/QuizChat.js
+++ src/components/QuizChat/QuizChat.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
+import { useSocket } from "../../contexts/SocketContext/SocketContext";
 
 const QuizChat = () => {
+  const socket = useSocket();
+
+  const handleChatSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("the-message", e.target.value);
+  };
+
+  useEffect(() => {
+    socket.on("chat-message", (username, msg) => {
+      return (
+        <li>
+          {username}: {msg}
+        </li>
+      );
+    });
+  }, [socket]);
   
 
   return (
     <div className=" mb-0 mt-16 rounded-lg overflow-hidden bg-slate-50 border-2 border-tblack p-4 flex flex-col items-center fixed w-96 h-2/3 z-10">
-      <form id="form" action="">
+      <form onSubmit={handleChatSubmit} id="form" action="">
         <input id="input" autoComplete="off" />
         <button>Send</button>
       </form>
