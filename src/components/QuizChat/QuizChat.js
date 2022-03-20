1e2bc802c09f8660fd8386950103a695a465c805
saminakhan999 <samina.khan9999@gmail.com>
2022-03-20 19:58:50 +0000

    return user and message

--- src/components/QuizChat/QuizChat.js
+++ src/components/QuizChat/QuizChat.js
@@ -1,15 +1,21 @@
-import React, {useEffect} from "react";
+import React, {useState} from "react";
 import { useSocket } from "../../contexts/SocketContext/SocketContext";
 
 const QuizChat = () => {
   const socket = useSocket();
+  const [input, setInput] = useState("");
 
   const handleChatSubmit = (e) => {
     e.preventDefault();
-    socket.emit("the-message", e.target.value);
+    socket.emit("the-message", input.value);
+    console.log("it worked");
+  };
+  const updateInput = (e) => {
+    const value = e.target.value;
+    setInput(value);
   };
 
-  useEffect(() => {
+  const onClick = () => {
     socket.on("chat-message", (username, msg) => {
       return (
         <li>
@@ -17,14 +23,20 @@ const QuizChat = () => {
         </li>
       );
     });
-  }, [socket]);
+    console.log("print message");
+  };
   
 
   return (
     <div className=" mb-0 mt-16 rounded-lg overflow-hidden bg-slate-50 border-2 border-tblack p-4 flex flex-col items-center fixed w-96 h-2/3 z-10">
-      <form onSubmit={handleChatSubmit} id="form" action="">
-        <input id="input" autoComplete="off" />
-        <button>Send</button>
+      <form onSubmit={handleChatSubmit} id="form">
+        <input
+          id="input"
+          value={input}
+          autoComplete="off"
+          onChange={updateInput}
+        />
+        <button onClick={onClick}>Send</button>
       </form>
     </div>
   );
